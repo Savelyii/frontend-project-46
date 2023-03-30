de04fa6bad7f9e7108631ced603df4464fc291f8
savelyii <leshaboichenko95@mail.ru>
2023-03-30 15:04:43 +0300

    update commander

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import _ from 'lodash';
+const genDiff = (filepath1, filepath2) => {
+  const key1 = Object.hasOwn(filepath1);
+  const key2 = Object.hasOwn(filepath2);
+  return;
+};
+export default genDiff;
