5d0a5a8ef95d7351e1d05556a61f4755760e7c5d
savelyii <leshaboichenko95@mail.ru>
2023-03-30 15:06:55 +0300

    update commander

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,5 +1,9 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
+import { readFileSync } from 'fs';
+import _ from 'lodash';
+import { cwd } from 'process';
+import genDiff from '../src/index.js';
 
 const program = new Command();
 
@@ -8,5 +12,9 @@ program
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1')
   .option('-f, --format <type>', 'output format')
-  .argument('<filepath1> <filepath2>');
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    const diff = genDiff(filepath1, filepath2);
+    console.log(diff);
+  });
 program.parse();
