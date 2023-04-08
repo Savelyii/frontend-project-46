3f9cb154904930c7986e8bb22a18423608086401
savelyii <leshaboichenko95@mail.ru>
2023-04-08 22:52:48 +0300

    fix lint and jest

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,11 +2,11 @@ import genDiff from '../src/index.js';
 
 test('function genDiff', () => {
   expect(genDiff('file1.json', 'file2.json')).toEqual(`{
-   - follow: false
-     host: hexlet.io
-   - proxy: 123.234.53.22
-   - timeout: 50
-   + timeout: 20
-   + verbose: true
-  }`);
+ - follow: false
+   host: hexlet.io
+ - proxy: 123.234.53.22
+ - timeout: 50
+ + timeout: 20
+ + verbose: true
+}`);
 });
