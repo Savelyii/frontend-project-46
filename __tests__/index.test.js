508e95304847825e2a368f01af96ffe51fc5ef36
savelyii <leshaboichenko95@mail.ru>
2023-04-14 20:24:15 +0300

    fix test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,6 +1,17 @@
 import genDiff from '../src/index.js';
 
-test('function genDiff', () => {
+test('function genDiff with json files', () => {
+  expect(genDiff('file1.json', 'file2.json')).toEqual(`{
+ - follow: false
+   host: hexlet.io
+ - proxy: 123.234.53.22
+ - timeout: 50
+ + timeout: 20
+ + verbose: true
+}`);
+});
+
+test('function genDiff with yml and yaml files', () => {
   expect(genDiff('file1.json', 'file2.json')).toEqual(`{
  - follow: false
    host: hexlet.io
