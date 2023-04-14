3b51daa65a49d0ca9823c4b3eac69b8be06f5ce1
savelyii <leshaboichenko95@mail.ru>
2023-04-14 19:19:05 +0300

    add parsers

--- src/parsers.js
+++ src/parsers.js
@@ -1,2 +1,14 @@
-import { readFileSync } from 'fs';
-import path from 'path';
+import yaml from 'js-yaml';
+
+const getParse = (data, format) => {
+  switch (format) {
+    case 'json':
+      return JSON.parse(data);
+    case 'yaml' || 'yml':
+      return yaml.load(data);
+    default:
+      throw new Error(`Format ${format} is not supported`);
+  }
+};
+
+export default getParse;
