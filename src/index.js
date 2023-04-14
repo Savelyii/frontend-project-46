c6eb0ad88523323b92448d7ed9eda6c50efe5650
savelyii <leshaboichenko95@mail.ru>
2023-04-14 20:08:21 +0300

    fix yml file

--- src/index.js
+++ src/index.js
@@ -2,16 +2,19 @@ import { readFileSync } from 'fs';
 import path from 'path';
 import buildTree from './buildTree.js';
 import stylish from './stylish.js';
+import getParse from './parsers.js';
 
 const genDiff = (filepath1, filepath2) => {
   const fixturesPath1 = path.resolve('__fixtures__', filepath1);
   const fixturesPath2 = path.resolve('__fixtures__', filepath2);
   const content1 = readFileSync(fixturesPath1, 'utf8');
   const content2 = readFileSync(fixturesPath2, 'utf8');
-  const obj1 = JSON.parse(content1);
-  const obj2 = JSON.parse(content2);
+  const fileEnd1 = path.extname(filepath1);
+  const fileEnd2 = path.extname(filepath2);
+  const obj1 = getParse(content1, fileEnd1);
+  const obj2 = getParse(content2, fileEnd2);
   const nodes = buildTree(obj1, obj2);
   return stylish(nodes);
 };
-console.log(genDiff('file1.json', 'file2.json'));
+console.log(genDiff('file1.yml', 'file2.yml'));
 export default genDiff;
--- src/parsers.js
+++ src/parsers.js
@@ -2,9 +2,10 @@ import yaml from 'js-yaml';
 
 const getParse = (data, format) => {
   switch (format) {
-    case 'json':
+    case '.json':
       return JSON.parse(data);
-    case 'yaml' || 'yml':
+    case '.yml':
+    case '.yaml':
       return yaml.load(data);
     default:
       throw new Error(`Format ${format} is not supported`);
